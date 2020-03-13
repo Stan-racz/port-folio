d019bb6820d67e26377875fe0485b32af62497dc
Stanislas Raczynski <stan.racz7@gmail.com>
2020-03-13 14:50:59 +0100

    Modification page développement, palmarès, et photos palmares veille sur index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,4 +15,52 @@ class IndexController extends AbstractController
     {
         return $this->render('index/index.html.twig');
     }
+
+    /**
+     * @Route("/apropos", name="apropos")
+     */
+    public function aPropos(): Response
+    {
+        return $this->render('index/apropos.html.twig');
+    }
+
+    /**
+     * @Route("/dev", name="dev")
+     */
+    public function developpement(): Response
+    {
+        return $this->render('index/developpement.html.twig');
+    }
+
+    /**
+     * @Route("/comp", name="competences")
+     */
+    public function competences(): Response
+    {
+        return $this->render('index/competences.html.twig');
+    }
+
+    /**
+     * @Route("/exp", name="exppro")
+     */
+    public function exppro(): Response
+    {
+        return $this->render('index/exppro.html.twig');
+    }
+
+    /**
+     * @Route("/veille", name="veilletech")
+     */
+    public function veilletech(): Response
+    {
+        return $this->render('index/veilletech.html.twig');
+    }
+
+    /**
+     * @Route("/palmares", name="palmares")
+     */
+    public function palmares(): Response
+    {
+        return $this->render('index/palmares.html.twig');
+    }
 }
